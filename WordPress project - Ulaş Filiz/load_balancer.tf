20cfcf8d68faeab33fc84b08011fd4a81cd1e832
Ulasus123 <51477699+Ulasus123@users.noreply.github.com>
2022-09-06 23:56:38 +0300

    Update load_balancer.tf
    loadBalancer ip outputting is disabled.

--- WordPress project - Ulaş Filiz/load_balancer.tf
+++ WordPress project - Ulaş Filiz/load_balancer.tf
@@ -42,8 +42,3 @@ resource "kubernetes_ingress_v1" "example" {
     }
   }
 }
-
-output "load_balancer_ip" {
-  value = kubernetes_ingress_v1.example.status[0].load_balancer[0].ingress[0].ip
-  description = "Deployment Wordpress IP"
-}
